d1d2d638ca25dc222696bbc3a4d1a505f33cdd55
Jordi Marín Valle <jordi.marin.valle@gmail.com>
2015-01-14 00:11:53 +0100

    redislabs free account credentials

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+
+# REDISLABS FREE ACCOUNT
+# https://redislabs.com/
+REDIS_HOST = 'pub-redis-13066.us-east-1-1.2.ec2.garantiadata.com'
+REDIS_PORT = '13066'
+REDIS_PASSWORD = 'eggbaconspam42'
+REDIS_DATABASE = '0'
