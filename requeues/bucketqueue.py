ab21f45f91369e66e1e415dc813003efe03335b3
Jordi Marín Valle <jordi.marin.valle@gmail.com>
2015-01-22 00:45:16 +0100

    BucketQueue queue interface created

---
+++ requeues/bucketqueue.py
@@ -0,0 +1,219 @@
+# -*- coding: utf-8 -*-
+
+import redis
+
+from requeues import QUEUE_COLLECTION_OF_ELEMENTS
+from requeues import KEEP_QUEUED_ELEMENTS_KEEP, KEEP_QUEUED_ELEMENTS_REMOVE
+
+from requeues import Tools
+from requeues.exceptions import RequeuesError
+from requeues.exceptions import RequeuesElementWithoutValueError
+
+
+class BucketQueue(object):
+    '''
+    A lightweight queue. Bucket Queue, uniqueness and super-fast queue
+    element checking.
+    '''
+
+    QUEUE_TYPE_NAME = 'bucket'
+
+    def __init__(self, id_args=[],
+                 collection_of=QUEUE_COLLECTION_OF_ELEMENTS,
+                 keep_previous=KEEP_QUEUED_ELEMENTS_KEEP,
+                 redis_conn=None):
+        '''
+        Create a SimpleQueue object.
+
+        Arguments:
+        :id_args -- list, list's values will be used to name the queue
+        :collection_of -- string (default: QUEUE_COLLECTION_OF_ELEMENTS),
+                          a type descriptor of queued elements
+        :keep_previous -- boolean (default: KEEP_QUEUED_ELEMENTS_KEEP),
+                          a flag to create a fresh queue or not
+        :redis_conn -- redis.client.Redis (default: None), a redis
+                       connection will be created using the default
+                       redis.client.Redis connection params.
+        '''
+        self.id_args = id_args
+        self.collection_of = collection_of
+
+        if redis_conn is None:
+            redis_conn = redis.Redis()
+        self.redis = redis_conn
+
+        self.key_queue_bucket = self.get_key_bucket()
+
+        if keep_previous is KEEP_QUEUED_ELEMENTS_REMOVE:
+            self.delete()
+
+    def __str__(self):
+        '''
+        Return a string representation of the class.
+
+        Returns: string
+        '''
+        return '<BucketQueue: %s (%s)>' % (self.key_queue_bucket, self.num())
+
+    def get_key_bucket(self):
+        '''
+        Get a key id that will be used to store/retrieve data from
+        the redis server.
+
+        Returns: string
+        '''
+        return 'queue:%s:type:%s:of:%s' % ('.'.join(self.id_args),
+                                           self.QUEUE_TYPE_NAME,
+                                           self.collection_of)
+
+    def push(self, element):
+        '''
+        Push a element into the queue.
+
+        Arguments:
+        :element -- string
+
+        Returns: long, the number of queued elements
+        '''
+        if element in ('', None):
+            raise RequeuesElementWithoutValueError()
+        return self._push_to_bucket(element)
+
+    def push_some(self, elements, num_block_size=None):
+        '''
+        Push a bunch of elements into the queue.
+
+        Arguments:
+        :elements -- a collection of strings
+        :num_block_size -- integer (default: none)
+
+        Returns: long, the number of queued elements
+        '''
+        return self._push_some_to_bucket(elements, num_block_size)
+
+    def pop(self):
+        '''
+        Pop a random element from the queue.
+
+        If no element is poped, it returns None
+
+        Arguments:
+        :last -- boolean (default: false)
+
+        Returns: string, the popped element, or, none, if no element is popped
+        '''
+        return self.redis.spop(self.key_queue_bucket)
+
+    def num(self):
+        '''
+        Get the number of elements that are queued.
+
+        Returns: integer, the number of elements that are queued
+        '''
+        return self.redis.scard(self.key_queue_bucket)
+
+    def is_empty(self):
+        '''
+        Check if the queue is empty.
+
+        Returns: boolean, true if queue is empty, otherwise false
+        '''
+        return True if self.num() is 0 else False
+
+    def is_not_empty(self):
+        '''
+        Check if the queue is not empty.
+
+        Returns: boolean, true if queue is not empty, otherwise false
+        '''
+        return not self.is_empty()
+
+    def is_element(self, element):
+        '''
+        Checks if a element is in the queue. It returns True is element
+
+        Arguments:
+        :element -- string
+
+        Returns: boolean
+        '''
+        return self.redis.sismember(self.key_queue_bucket, element)
+
+    def elements(self, num_elements=-1):
+        '''
+        Get some (or even all) unordered queued elements.
+        By default it returns all queued elements.
+
+        Note
+        ====
+        Elements are not popped.
+
+        Arguments:
+        :num_elements -- integer (default: -1).
+
+        Returns: set
+        '''
+        if num_elements is -1:
+            return self.redis.smembers(self.key_queue_bucket)
+        return set(self.redis.srandmember(self.key_queue_bucket, num_elements))
+
+    def delete(self):
+        '''
+        Delete the queue with all its elements.
+
+        Returns: boolean, true if queue has been deleted, otherwise false
+        '''
+        return True if self.redis.delete(self.key_queue_bucket) else False
+
+    def _push_to_bucket(self, element):
+        '''
+        Push a element into the queue.
+
+        Arguments:
+        :element -- string
+
+        Raise:
+        :RequeuesError(), if element can not be pushed
+
+        Returns: long, the number of queued elements
+        '''
+        try:
+            return self.redis.sadd(self.key_queue_bucket, element)
+        except Exception as e:
+            raise RequeuesError(e.message)
+
+    def _push_some_to_bucket(self, elements, num_block_size=None):
+        '''
+        Push a bunch of elements into the queue.
+
+        Arguments:
+        :elements -- a collection of strings
+        :num_block_size -- integer (default: none)
+
+        Raise:
+        :RequeuesError(), if elements can not be pushed
+
+        Returns: number, the number of queued elements
+        '''
+        try:
+
+            elements = list(elements)
+
+            block_slices = Tools.get_block_slices(
+                num_elements=len(elements),
+                num_block_size=num_block_size
+            )
+
+            pipe = self.redis.pipeline()
+            for s in block_slices:
+                some_elements = elements[s[0]:s[1]]
+                pipe.sadd(self.key_queue_bucket, *some_elements)
+
+            num_pushed_elements = 0
+            for result in pipe.execute():
+                num_pushed_elements += result
+            return num_pushed_elements
+
+        except Exception as e:
+            raise RequeuesError(e.message)
+
---
+++ tests/test_bucketqueue.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+import random
+
+from requeues import KEEP_QUEUED_ELEMENTS_REMOVE
+
+from tests import redis_conn
+from requeues.bucketqueue import BucketQueue
+
+
+ELEMENT_EGG = b'egg'
+ELEMENT_BACON = b'bacon'
+ELEMENT_SPAM = b'spam'
+ELEMENT_42 = b'42'
+
+ELEMENT_UNEXISTENT_ELEMENT = b'utopia'
+
+some_elements = [
+    ELEMENT_EGG,
+    ELEMENT_BACON,
+    ELEMENT_SPAM,
+    ELEMENT_SPAM,
+    ELEMENT_SPAM,
+    ELEMENT_42,
+    ELEMENT_SPAM,
+]
+
+
+class TestBucketQueue(object):
+
+    def setup(self):
+        self.queue = BucketQueue(
+            id_args=['test', 'testing'],
+            redis_conn=redis_conn
+        )
+
+    def test_empty(self):
+        assert self.queue.num() is 0
+        assert self.queue.is_empty() is True
+        assert self.queue.is_not_empty() is False
+
+    def test_push(self):
+        assert self.queue.push(ELEMENT_EGG) is 1
+
+    def test_push_some(self):
+        assert self.queue.push_some(some_elements) is len(set(some_elements))
+
+    def test_pop(self):
+        self.queue.push_some(some_elements)
+        assert self.queue.pop() != None
+
+    def test_pop_empty_queue(self):
+        assert self.queue.pop() is None
+
+    def test_is_element(self):
+        self.queue.push_some(some_elements)
+        assert self.queue.is_element(some_elements[0]) is True
+
+    def test_is_not_element(self):
+        self.queue.push_some(some_elements)
+        assert self.queue.is_element(ELEMENT_UNEXISTENT_ELEMENT) is False
+
+    def test_elements(self):
+        self.queue.push_some(some_elements)
+        elements = self.queue.elements()
+        for i, some_element in enumerate(list(set(some_elements))):
+            elements.discard(some_element)
+        assert len(elements) is 0
+
+    def test_n_elements(self):
+        self.queue.push_some(some_elements)
+        num_remaining = 1
+        elements = self.queue.elements(len(set(some_elements)) - num_remaining)
+        assert len(set(some_elements).difference(elements)) is num_remaining
+
+    def test_fresh_queue(self):
+        self.queue.push(ELEMENT_EGG)
+        assert self.queue.is_not_empty() is True
+
+        queue_y = BucketQueue(
+            id_args=['test', 'testing'],
+            keep_previous=KEEP_QUEUED_ELEMENTS_REMOVE,
+            redis_conn=redis_conn
+        )
+        assert queue_y.is_empty() is True
+
+    def test_delete(self):
+        self.queue.push(element=ELEMENT_42)
+        assert self.queue.num() == 1
+        assert self.queue.delete() is True
+        assert self.queue.num() == 0
+
+    def teardown(self):
+        self.queue.delete()
+
+
+if __name__ == '__main__':
+    pytest.main()
