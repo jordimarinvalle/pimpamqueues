9db6d40fc21445da187df64b9e23591008fa09c9
Jordi Marín Valle <jordi.marin.valle@gmail.com>
2015-01-23 20:27:25 +0100

    refactored and improved unit tests

--- tests/test_simplequeue.py
+++ tests/test_simplequeue.py
@@ -19,6 +19,7 @@ some_elements = [
     ELEMENT_BACON,
     ELEMENT_SPAM,
     ELEMENT_42,
+    ELEMENT_SPAM,
 ]
 
 
@@ -36,65 +37,39 @@ class TestSimpleQueue(object):
         assert self.queue.is_not_empty() is False
 
     def test_push(self):
-        assert self.queue.push(ELEMENT_EGG) > 0
-        assert self.queue.num() is 1
-        assert self.queue.is_empty() is False
-        assert self.queue.is_not_empty() is True
+        assert self.queue.push(ELEMENT_EGG) == 1
 
-    def test_push_pop(self):
-        assert self.queue.push(ELEMENT_EGG) > 0
-        assert self.queue.pop() == ELEMENT_EGG
-
-    def test_pop_empty_queue(self):
-        assert self.queue.pop() is None
-
-    def test_push_first_pop_last(self):
-        self.queue.push(element=ELEMENT_EGG, queue_first=False)
-        self.queue.push(element=ELEMENT_BACON, queue_first=False)
-        self.queue.push(element=ELEMENT_SPAM, queue_first=False)
-        self.queue.push(element=ELEMENT_42, queue_first=True)
-
-        assert self.queue.elements()[-1] == ELEMENT_SPAM
-        assert self.queue.pop(last=True) == ELEMENT_SPAM
-
-    def test_push_pop_last(self):
-        self.queue.push(element=ELEMENT_EGG)
-        self.queue.push(element=ELEMENT_BACON)
-        self.queue.push(element=ELEMENT_SPAM)
-        self.queue.push(element=ELEMENT_42)
-
-        assert self.queue.elements()[0] == ELEMENT_EGG
-        assert self.queue.pop() == ELEMENT_EGG
+    def test_push_to_first(self):
+        self.queue.push(ELEMENT_EGG)
+        self.queue.push(ELEMENT_BACON)
+        self.queue.push(ELEMENT_SPAM)
+        self.queue.push(ELEMENT_42, queue_first=True)
+        assert self.queue.pop() == ELEMENT_42
 
     def test_push_some(self):
-        assert self.queue.num() is 0
         assert self.queue.push_some(some_elements) == len(some_elements)
-        assert self.queue.num() is len(some_elements)
 
-    def test_push_some_to_beginning(self):
-        self.queue.push(element=ELEMENT_42, queue_first=True)
-        self.queue.push_some(elements=some_elements, queue_first=True)
-        assert self.queue.num() == 1 + len(some_elements)
+    def test_push_some_to_first(self):
+        self.queue.push(ELEMENT_42)
+        self.queue.push_some(elements=[ELEMENT_EGG, ELEMENT_BACON],
+                             queue_first=True)
         assert self.queue.pop() == ELEMENT_EGG
+
+    def test_pop(self):
+        self.queue.push(ELEMENT_EGG)
+        assert self.queue.pop() == ELEMENT_EGG
+
+    def test_pop_none(self):
+        assert self.queue.pop() is None
 
     def test_elements(self):
         self.queue.push_some(some_elements)
-        assert self.queue.elements() == some_elements
+        elements = self.queue.elements()
+        assert len(set(some_elements).difference(set(elements))) is 0
 
-    def test_first_elements(self):
+    def test_elements_first_elements(self):
         self.queue.push_some(some_elements)
         assert self.queue.first_elements(3) == some_elements[0:3]
-
-    def test_fresh_queue(self):
-        self.queue.push(ELEMENT_EGG)
-        assert self.queue.is_not_empty() is True
-
-        queue_y = SimpleQueue(
-            id_args=['test', 'testing'],
-            keep_previous=KEEP_QUEUED_ELEMENTS_REMOVE,
-            redis_conn=redis_conn
-        )
-        assert queue_y.is_empty() is True
 
     def test_delete(self):
         self.queue.push(element=ELEMENT_42)
@@ -102,9 +77,19 @@ class TestSimpleQueue(object):
         assert self.queue.delete() is True
         assert self.queue.num() == 0
 
+    def test_queue_new_queue_remove_queued_elements(self):
+        self.queue.push(ELEMENT_EGG)
+        assert self.queue.is_not_empty() is True
+
+        queue = SimpleQueue(
+            id_args=['test', 'testing'],
+            keep_previous=KEEP_QUEUED_ELEMENTS_REMOVE,
+            redis_conn=redis_conn
+        )
+        assert queue.is_empty() is True
+
     def teardown(self):
         self.queue.delete()
-
 
 if __name__ == '__main__':
     pytest.main()
