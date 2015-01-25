641802bbf8b5c96899d12299b60d22a35e471a17
Jordi Marín Valle <jordi.marin.valle@gmail.com>
2015-01-25 20:03:36 +0100

    included a test case that num_elements is zero

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from pimpamqueues import Tools
 
@@ -12,6 +12,7 @@ class TestTools(object):
         self.block_slices_27_10 = Tools.get_block_slices(27, 10)
         self.block_slices_13_2 = Tools.get_block_slices(13, 2)
         self.block_slices_3_141592 = Tools.get_block_slices(3, 141592)
+        self.block_slices_0_1000 = Tools.get_block_slices(0, 1000)
 
     def test_block_slices(self):
         assert len(self.block_slices_300_1) == 300
@@ -19,6 +20,7 @@ class TestTools(object):
         assert len(self.block_slices_27_10) == 3
         assert len(self.block_slices_13_2) == 7
         assert len(self.block_slices_3_141592) == 1
+        assert len(self.block_slices_0_1000) == 1
 
     def test_block_slices_a_slice(self):
         assert self.block_slices_300_1[0] == [0, 1]
@@ -27,6 +29,7 @@ class TestTools(object):
         assert self.block_slices_27_10[2] == [20, 30]
         assert self.block_slices_13_2[1] == [2, 4]
         assert self.block_slices_3_141592[0] == [0, 3]
+        assert self.block_slices_0_1000[0] == [0, 0]
 
 
 if __name__ == '__main__':
