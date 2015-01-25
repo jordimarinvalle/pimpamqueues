8e19b0c0d7f05c2c6c05257ea73fd68fb89b5555
Jordi Marín Valle <jordi.marin.valle@gmail.com>
2015-01-25 19:56:32 +0100

    testing Redis endpoint (host, port) changed

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import redis
 
 # REDISLABS FREE ACCOUNT
 # https://redislabs.com/
-REDIS_HOST = 'pub-redis-13066.us-east-1-1.2.ec2.garantiadata.com'
-REDIS_PORT = '13066'
+REDIS_HOST = 'pub-redis-11028.us-east-1-1.2.ec2.garantiadata.com'
+REDIS_PORT = '11028'
 REDIS_PASSWORD = 'eggbaconspam42'
 REDIS_DATABASE = '0'
 
