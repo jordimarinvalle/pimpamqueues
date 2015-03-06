66ca21e8f8dfb73ac59b5c9cbadee6a4d47833d8
Jordi Marín Valle <jordi.marin.valle@gmail.com>
2015-03-06 00:11:42 -0800

    redislabs misplaced credentials..

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,8 +5,8 @@ import redis
 
 # REDISLABS FREE ACCOUNT
 # https://redislabs.com/
-REDIS_HOST = 'pub-redis-12294.us-east-1-1.1.ec2.garantiadata.com:12294'
-REDIS_PORT = '11028'
+REDIS_HOST = 'pub-redis-12294.us-east-1-1.1.ec2.garantiadata.com'
+REDIS_PORT = '12294'
 REDIS_PASSWORD = 'eggbaconspam42'
 REDIS_DATABASE = '0'
 
